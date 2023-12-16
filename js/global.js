c298d323b2f67e83f59e1fc3119384899d85d66c
mauicruz <143379997+mauiicruz@users.noreply.github.com>
2023-12-17 01:19:28 +0800

    commit

---
+++ js/global.js
@@ -0,0 +1,37 @@
+let modeRelax = false;
+let movement = 0;
+let stopwatch;
+let cardGroup = [
+    ["🎁","🎉"],
+    ["🌟","☃️"],
+    ["🐯","🐼","🐶","🐱"],
+    ["🤩","🤑","🫰","🫶"],
+    ["🃏","🏀","👻","👾"]
+   
+
+    ];
+
+let levelActual = 0;
+let levels =[
+    {
+        cards:cardGroup[0],
+        movementMax: 4
+    },
+    {
+        cards:cardGroup[0].concat(cardGroup[1]),
+        movementMax: 10
+    },
+    {
+        cards:cardGroup[0].concat(cardGroup[1], cardGroup[2]),
+        movementMax: 14
+    },
+    {
+        cards:cardGroup[0].concat(cardGroup[1], cardGroup[2],cardGroup[3]),
+        movementMax: 25
+    },
+    {cards:cardGroup[0].concat(cardGroup[1],cardGroup[2],cardGroup[3],cardGroup[4]),
+    movementMax:60
+    
+    }
+
+];
